1b753d80d5fb66c3958189074461273d2ba5764d
jujdu <sidoruk.misha@gmail.com>
2019-12-22 21:51:10 +0300

    start note project

---
+++ UnCloudNotes/UnCloudNotes_starter/UnCloudNotes/Attachment.swift
@@ -0,0 +1,17 @@
+//
+//  Attachment.swift
+//  UnCloudNotes
+//
+//  Created by Michael Sidoruk on 22.12.2019.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class Attachment: NSManagedObject {
+  @NSManaged var dateCreated: Date
+  @NSManaged var image: UIImage?
+  @NSManaged var note: Note?
+}
